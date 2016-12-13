039e9e4871b1c4e40bb0e3e8ca4ba779c678da92
Andrmd <ukai_keisuke@yse-c.net>
2016-12-13 15:12:46 +0900

    Aloha.java completion!

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,12 +1,20 @@
 public class Aloha{
   public static void main(String[] args) {
-    for (int i=0; i<20; i++ ) {
-      if (i%3) {
-        System.out.println("Aloha!");
-      }else{
-        System.out.println("アロハ!");
+    String mainMsg;
+    String subMsg;
+    mainMsg = "test";
+    subMsg = "test";
+
+
+while (true) {
+    System.out.println("Please message");
+    mainMsg = new java.util.Scanner(System.in).nextLine();
+    if(mainMsg.equals("exit")) {
+      System.out.println("exit!");
+      break;
+    }else{
+      System.out.println("Your massage:"+mainMsg);
       }
     }
-
   }
 }
