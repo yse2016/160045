30a0205f6af49bade4a9cf8686fb3dcdaa119082
Andrmd <ukai_keisuke@yse-c.net>
2017-01-30 12:15:27 +0900

    updated

---
+++ 20170119/Idean.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+// buttonx2
+// savebutton
+//disp x2
+//memo input
+
+class IdeaMan implements ActionListener{
+
+  public IdeanMan(){
+    //newframe
+    frame = new JFrame("Idean");
+    frame.setLocation(600,250);
+    frame.setSize(512,512);
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+}
